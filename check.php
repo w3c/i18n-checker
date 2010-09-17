94c036f7d18529d43026ef8410be610fde6fc396
Thomas Gambet <tgambet@w3.org>
2010-09-17 14:37:52 -0400

    + xml output

--- check.php
+++ check.php
@@ -1,16 +1,12 @@
 <?php
 require_once('code/common.php');
-/*include('code/initialcode.php');
-include('code/n11n.php');
-include('code/checkercode.php');
-include('code/checkermessages.php');
-include('code/createmessages.php');*/
-
 include('code/initialcode.php');
 include('code/n11n.php');
 include('code/checkercode.php');
 include('code/checkermessages.php');
 include('code/createmessages.php');
-
-include('templates/results.html.php');
-
+if (isset($_REQUEST['format']) && $_REQUEST['format'] == 'xml') {
+	include('templates/results.xml.php');
+} else {
+	include('templates/results.html.php');
+}
