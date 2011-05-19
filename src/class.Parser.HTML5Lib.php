4ae11710b40715df06f4b5ef602f10b7329094ae
tgambet <tgambet@localhost>
2011-05-19 18:00:51 -0400

    + parse for Content-Language meta tags

--- src/class.Parser.HTML5Lib.php
+++ src/class.Parser.HTML5Lib.php
@@ -14,6 +14,32 @@ final class ParserHTML5Lib extends Parser {
 		parent::__construct($markup, $contentType);
 	}
 	
+	// XXX: if phpQuery::loadDocument was implemented could be refactored in Parser. eg:
+	// phpQuery::loadDocument($this->document);
+	// pq('meta[http-equiv=content-language]');
+	protected function parseMeta() {
+		$this->charsetsFromHTML = array();
+		$this->metaCharsetTags = array();
+		$this->langsFromMeta = array();
+		$this->metaLanguageTags = array();
+		$metas = $this->document->getElementsByTagName("meta");
+		foreach ($metas as $meta) {
+			// check for charset attribute
+			if (($charset = $meta->attributes->getNamedItem('charset')) != null) {
+				$this->charsetsFromHTML[] = strtoupper($charset->value);
+				$this->metaCharsetTags[] = $this->dump($meta);
+			// check for http-equiv="content-language" (deprecated in HTML5)
+			// TODO: Add a warning if <meta http-equiv="content-language" content="en"> is used in HTML5?
+			// TODO: Content-Language value should be split on ',' and not used as-is
+			// FIXME: case sensitity of getNamedItem
+			} else if (($equivParam = $meta->attributes->getNamedItem('http-equiv')) != null
+						&& strcasecmp($equivParam->value, 'Content-Language') == 0) {
+				if (($contentParam = $meta->attributes->getNamedItem('content')) != null)
+					$this->langsFromMeta[] = $contentParam->value;
+				$this->metaLanguageTags[] = $this->dump($meta);
+			}
+		}
+	}
 }
 
 ParserHTML5Lib::init();
--- src/class.Parser.PHPQuery.php
+++ src/class.Parser.PHPQuery.php
@@ -17,24 +17,53 @@ final class ParserPHPQuery extends Parser {
 		parent::__construct($markup, $contentType);
 	}
 	
-	// @Override
 	protected function parseMeta() {
 		$this->charsetsFromHTML = array();
 		$this->metaCharsetTags = array();
-		// Seems we can't use an anonymous callback function
-		pq('meta[http-equiv=Content-Type]')->each(array($this, 'addCharsetHTML'));
+		$this->langsFromMeta = array();
+		$this->metaLanguageTags = array();
+		
+		// XXX: attributes values in selectors are case sensitive!
+		// ---- Solution1: 
+		// pq('meta[http-equiv]')->each(array($this, 'publicCallbackFunction'));
+		// pq('meta[http-equiv]')->each(array($this, 'publicCallbackFunction'));
+		
+		// ---- Solution2: Requires PHP5.3 closures.
+		/*$doc = &$this->document;
+		$tags = &$this->metaCharsetTags;
+		$charsets = &$this->charsetsFromHTML;
+		$charsetCallback = function ($node) use (&$tags, &$charsets, &$doc) {
+				$contentType = Utils::contentTypeToArray(pq($node)->attr('content'));
+				$charsets[] = strtoupper($contentType['charset']);
+				$tags[] = $doc->saveXML($node);
+			};
+		pq('meta[http-equiv=Content-Type]')->each($charsetCallback);
+		pq('meta[http-equiv=content-type]')->each($charsetCallback);
+		$tags = &$this->$metaLanguageTags;
+		$langs = &$this->langsFromMeta;
+		$langCallback = function ($node) use (&$tags, &$langs, &$doc) {
+				$langs[] = pq($node)->attr('content');
+				$tags[] = $doc->saveXML($node);
+			};
+		pq('meta[http-equiv="Content-Language"]')->each($langCallback);
+		pq('meta[http-equiv="content-language"]')->each($langCallback);*/
+		
+		// ---- Solution3: fixes the case sensitivity issue on Content-Type
+		// FIXME: case sensitivity on http-equiv
+		// TODO: Content-Language value should be split on ',' and not used as-is
+		foreach (pq('meta[http-equiv]') as $meta) {
+			if (strcasecmp(pq($meta)->attr('http-equiv'), 'Content-Type') == 0) {
+				$contentType = Utils::contentTypeToArray(pq($meta)->attr('content'));
+				$this->charsetsFromHTML[] = strtoupper($contentType['charset']);
+				$this->metaCharsetTags[] = $this->dump($meta);
+			}
+			else if (strcasecmp(pq($meta)->attr('http-equiv'), 'Content-Language') == 0) {
+				$this->langsFromMeta[] = pq($meta)->attr('content');
+				$this->metaLanguageTags[] = $this->dump($meta);
+			}
+		}
 	}
 	
-	public function addCharsetHTML($node) {
-		$contentType = Utils::contentTypeToArray(pq($node)->attr('content'));
-		$this->charsetsFromHTML[] = strtoupper($contentType['charset']);
-		$this->metaCharsetTags[] = $this->dump($node);
-		self::$logger->debug("Found meta tag charset: ".$contentType['charset']);
-	}
-	
-	/*public function langFromHTML() {
-		self::$logger->debug($this->dumpTag(pq('html')->elements[0]));
-	}*/
 }
 
 ParserPHPQuery::init();
