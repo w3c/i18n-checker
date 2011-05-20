9ccbfd891d887addb1ce7440866b2961809435df
tgambet <tgambet@localhost>
2011-05-20 14:47:20 -0400

    + implemented

--- src/class.Report.php
+++ src/class.Report.php
@@ -1,17 +1,67 @@
 <?php
 
+define("REPORT_LEVEL_INFO", "info");
+define("REPORT_LEVEL_WARNING", "warning");
+define("REPORT_LEVEL_ERROR", "error");
+
 class Report {
 	
+	private static $logger;
 	public static $reports;
 	
+	public $category;
 	public $severity;
+	public $title;
+	public $explanation;
+	public $whattodo;
+	public $further;
 	
-	public function __construct() {
-		
+	public static function init() {
+		self::$logger = Logger::getLogger('Report');
 	}
 	
-	public static function addReport() {
+	private function __construct($category, $severity, $title, $explanation, $whattodo, $further) {
+		$this->category = $category;
+		$this->severity = $severity;
+		$this->title = $title;
+		$this->explanation = $explanation;
+		$this->whattodo = $whattodo;
+		$this->further = $further;
+	}
+	
+	public static function addReport($category, $severity, $title, $explanation, $whattodo, $further) {
+		self::$reports[] = new Report($category, $severity, $title, $explanation, $whattodo, $further);
+	}
+	
+	public static function getCount() {
+		return count(self::$reports);
+	}
+	
+	public static function getCountFor($severity) {
+		$callBack = function($report) use ($severity) {
+			if ($report->severity == $severity)
+				return true;
+			return false;
+		};
+		return count(array_filter(self::$reports, $callBack));
+	}
+	
+	public static function getErrorCount() {
+		return self::getCountFor(REPORT_LEVEL_ERROR);
+	}
+	
+	public static function getWarningCount() {
+		return self::getCountFor(REPORT_LEVEL_WARNING);
+	}
+	
+	public static function getInfoCount() {
+		return self::getCountFor(REPORT_LEVEL_INFO);
+	}
+	
+	private static function filterReports($severity) {
 		
 	}
 	
 }
+
+Report::init();
