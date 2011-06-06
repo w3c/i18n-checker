1d30673be7c15e80987d6bd8c6ad826a47490f42
tgambet <tgambet@localhost>
2011-06-06 18:04:32 -0400

    ~ reviewed data structure to be able to link a value to a specific line of code

--- src/class.Information.php
+++ src/class.Information.php
@@ -6,50 +6,69 @@ class Information {
 	
 	public $category;
 	public $title;
-	public $value;
+	public $values;
+	/* eg:
+	 * $values = array (
+	 * 		array (
+	 * 			'code' => '<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />'
+	 * 			'values' => 'utf-8'
+	 * 		),
+	 *		array (
+	 * 			'code' => 'Content-Language: ka, ta'
+	 * 			'values' => array (ka, ta);
+	 * 		),
+	 * 		array (
+	 * 			'code' => null
+	 * 			'values' => null;
+	 * 		),
+	 * ) 
+	 */
 	public $display_value;
-	public $code;
-	// Not used: set category to null to hide that information from the info table
-	public $hidden;
 	
-	private function __construct($category, $title, $value, $display_value, $code, $hidden) {
+	private function __construct($category, $title, $values, $display_value = null) {
 		$this->category = $category;
 		$this->title = $title;
-		$this->value = $value;
+		$this->values = $values;
 		$this->display_value = $display_value;
-		$this->code = $code;
-		$this->hidden = $hidden;
 	}
 	
-	public static function addInfo($category, $title, $value, $display_value, $code, $hidden = false) {
-		self::$infos[] = new Information($category, $title, $value, $display_value, $code, $hidden);
+	public static function addInfo($category, $title, $values, $display_value) {
+		if (is_array($values)) {
+			if (array_key_exists('values', $values)) {
+				$values = array($values);
+			}
+		} else {
+			$values = array(
+				array(
+					'code' => null,
+					'values' => $values
+				)
+			);
+		}
+		self::$infos[] = new Information($category, $title, $values, $display_value);
 	}
 	
 	public static function getInfoPerCategory() {
-		foreach (self::$infos as $info) {
-			if ($info->category != null) {
+		foreach (self::$infos as $info)
+			if ($info->category != null)
 				$result[$info->category][] = $info;
-			}
-		}
 		return $result;
 	}
 	
 	public static function get($name) {
-		foreach (self::$infos as $info) {
-			if ($info->title == $name) {
+		foreach (self::$infos as $info)
+			if ($info->title == $name)
 				return $info;
-			}
-		}
 		return null;
 	}
 	
-	public static function getValue($name) {
-		return self::get($name) ? self::get($name)->value : null; 
-	}
+	/*public static function getValues($name) {
+		return self::get($name) ? self::get($name)->values : null; 
+	}*/
 	
-	public static function getCode($name) {
+	/*public static function getCode($name) {
 		return self::get($name) ? self::get($name)->code : null; 
-	}
+	}*/
 	
 	public static function getCount() {
 		return count(self::$infos);
