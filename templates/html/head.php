2ed105f699a0aece94fa4b19749e4b297f0ba5d5
tgambet <tgambet@localhost>
2011-06-17 22:48:09 -0400

    ~ removed output buffer cleaning

--- templates/html/head.php
+++ templates/html/head.php
@@ -1,10 +1,3 @@
-<?php
-if (ob_get_length() > 0 && !Conf::get('debug')) {
-	$logger = Logger::getLogger('Output');
-	$logger->error("Output buffer is not empty: \n".ob_get_clean());
-}
-$logger = Logger::getLogger('Templates');
-?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $lang ?>" lang="<?php echo $lang ?>">
 <head>
