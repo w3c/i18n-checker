76f5a155385239ed0b5654c0a8e3d089ae3e07e0
tgambet <tgambet@localhost>
2011-06-27 08:54:50 -0400

    - dead links to stylesheets

--- www/feedback.php
+++ www/feedback.php
@@ -3,8 +3,6 @@ require_once(realpath(dirname(__FILE__).'/../src/class.Conf.php'));
 require_once(PATH_SRC.'/class.Language.php');
 require_once(PATH_SRC.'/class.Message.php');
 $title = "W3C I18n Checker";
-$css[] = "base_ucn.css";
-$css[] = "minimum.css";
 include(PATH_TEMPLATES.'/html/head.php');
 ?>
 
