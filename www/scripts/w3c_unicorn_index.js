766a2c3cfbc93ad58850e1613df19d692f6ba71d
tgambet <tgambet@localhost>
2011-06-27 08:52:38 -0400

    ~ mootools updated to 1.3.2 - dependencies on Form.Validator + prepared some asynchronous functionnality

--- www/scripts/w3c_unicorn_index.js
+++ www/scripts/w3c_unicorn_index.js
@@ -5,7 +5,7 @@ var W3C = {
 	start: function(){
 		
 		W3C.Tabs = $('tabset_tabs');
-		W3C.TabLinks = W3C.Tabs.getChildren('li a');
+		W3C.TabLinks = W3C.Tabs.getChildren('li');
 		W3C.LanguagesForm = $('lang_choice');
 		W3C.LangParameter = $$('html').getProperty('lang')[0];
 		W3C.Forms = $$('form.ucn_form');
@@ -18,7 +18,8 @@ var W3C = {
 		W3C.parseHash();
 		//W3C.updateHash();
 		W3C.showTab(W3C.SelectedTab, false);
-		console.log(window.location);
+		W3C.Loader = new Element('img', {'src': './images/ajax-loader.gif', 'class': 'loader'});
+		//console.log(window.location);
 	},
 	
 	prepareDocument: function(){
@@ -40,31 +41,89 @@ var W3C = {
 		});
 		
 		W3C.Forms.filter('form[method=get]').each(function (form) {
-			new FormValidator(form, {
+			/*form.getElement('input[type=submit]').addEvent('click', function(event) {
+				event.preventDefault();
+			});*/
+			form.addEvent('submit', function(event) {
+				event.preventDefault();
+				//form.uri.value = form.uri.value.replace('http://', '');
+				//form.submit();
+				// ------- With ajax
+				/*if ($('messages'))
+					$('messages').dispose();
+				if ($$('div.intro'))
+					$$('div.intro').dispose();
+				if ($$('body > div.section'))
+					$$('body > div.section').dispose();
+				// Generate the query string
+				var queryString = form.toQueryString().replace('uri=http%3A%2F%2F', 'uri=');
+				// Create the request and send
+				var formRequest = new Request.HTML({
+					url: W3C.Action + "?" + queryString,
+					method: 'get',
+					onRequest: function() {
+						W3C.Loader.inject(W3C.Forms[W3C.SelectedTab].getElement('div.input'),'before');
+					},
+					onSuccess: function(responseTree, responseElements, responseHTML, responseJavaScript) {
+						$$('img.loader').dispose();
+						//console.log(responseHTML);
+						//responseTree.set('')
+						//var t = new Element('div').set('html', responseHTML);//.inject($('frontforms'), 'after');
+						//t.inject($('frontforms'), 'after');
+						console.log(responseHTML);
+						console.log($$(responseHTML));
+						$$(responseTree).inject($('don_program'), 'before');
+						// Load and start the result script
+						var myScript = Asset.javascript('./scripts/w3c_unicorn_results.js', {
+						    id: 'w3c_unicorn_results',
+						    onLoad: function(){
+						    	W3Cr.start();
+						    }
+						});
+						return true;
+					},
+					onFailure: function() {
+						// TODO
+						$$('img.loader').dispose();
+						return false;
+					}
+				});
+				formRequest.send();*/
+				// ------- Without ajax
+				var queryString = form.toQueryString().replace('uri=http%3A%2F%2F', 'uri=');
+				if ("?" + queryString == window.location.search) {
+					window.location.reload(true);
+				} else {
+					window.location = W3C.Action + "?" + queryString + "#" + W3C.getHash();
+				}
+			});
+			/*new FormValidator(form, {
 				onFormValidate: function(passed, form, event) {
 					if (passed) {
 						event.preventDefault();
+						//form.submit();
 						var queryString = form.toQueryString().replace('uri=http%3A%2F%2F', 'uri=');
-
 						if ("?" + queryString == window.location.search) {
 							window.location.reload(true);
 						} else {
 							window.location = W3C.Action + "?" + queryString + "#" + W3C.getHash();
 						}
-						
 					}
 				}
-			});
+			});*/
 		});
 		
 		W3C.Forms.filter('form[method=post]').each(function (form) {
-			new FormValidator(form, {
+			form.addEvent('submit', function(event) {
+				form.setProperty('action', form.getProperty('action') + '#' + W3C.getHash());
+			})
+			/*new FormValidator(form, {
 				onFormValidate: function(passed, form, event) {
 					if (passed) {
 						form.setProperty('action', form.getProperty('action') + '#' + W3C.getHash());
 					}
 				}
-			});
+			});*/
 		});
 		
 	},
