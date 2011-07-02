402b3e596c7aa272b246084caad0d1d19d1cbdc5
tgambet <tgambet@localhost>
2011-07-02 05:56:04 -0400

    + detection of the mimetype parameter to be used to force a specific mimetype

--- www/check.php
+++ www/check.php
@@ -5,6 +5,17 @@ require_once(PATH_SRC.'/class.Message.php');
 // Check debug_lang parameter
 if (isset($_GET['debug_lang']) && $_GET['debug_lang'] == 'true')
 	Conf::set('debug_lang', 'true');
+// Check mimetype parameter
+if (isset($_REQUEST['mimetype']) && $_REQUEST['mimetype'] != '') {
+	switch ($_REQUEST['mimetype']) {
+		case "html":
+			$forcedMimeType = "text/html";
+			break;
+		case "xml":
+			$forcedMimeType = "application/xhtml+xml";
+			break;
+	}
+}
 // Check format parameter
 $format = isset($_REQUEST['format']) && $_REQUEST['format'] != "" ? $_REQUEST['format'] : Conf::get('default_format');
 if (!file_exists(PATH_TEMPLATES."/results.$format.php")) {
@@ -35,7 +46,7 @@ if (isset($_GET['debug_upload']) && $_GET['debug_upload'] == 'true'){
 	$curl_info = null;
 }
 $checker = new Checker($curl_info, $content);
-if (!$checker->checkDocument()) {
+if (!$checker->checkDocument($forcedMimeType)) {
 	include(PATH_TEMPLATES."/index.$format.php");
 	exit;
 }
