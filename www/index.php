6ea09fadce05f6ddecd8527833db6b406f0ae6ae
tgambet <tgambet@localhost>
2013-04-25 16:35:59 +0200

    - "pre-finale release" comment

--- www/index.php
+++ www/index.php
@@ -5,7 +5,7 @@ require_once(PATH_SRC.DIRECTORY_SEPARATOR.'class.Message.php');
 // Hide language selection for now - remove this line or set to false to bring it back
 $hideLangSelection = true;
 
-Message::addMessage(MSG_LEVEL_INFO, 'This is a pre-final release of the checker. Please contact us about errors, bugs or suggestions using the <a href="http://www.w3.org/International/2007/06/surveyform-110707.php?docname=http://validator.w3.org/i18n-checker&amp;referer=http://validator.w3.org/i18n-checker">feedback form</a>. We already have plans to add further tests and features, to translate the user interface, to add support for XHTML5 and polyglot documents, and to integrate with the W3C Unicorn checker.');
+//Message::addMessage(MSG_LEVEL_INFO, 'This is a pre-final release of the checker. Please contact us about errors, bugs or suggestions using the <a href="http://www.w3.org/International/2007/06/surveyform-110707.php?docname=http://validator.w3.org/i18n-checker&amp;referer=http://validator.w3.org/i18n-checker">feedback form</a>. We already have plans to add further tests and features, to translate the user interface, to add support for XHTML5 and polyglot documents, and to integrate with the W3C Unicorn checker.');
 
 $format = isset($_REQUEST['format']) && $_REQUEST['format'] != "" ? $_REQUEST['format'] : Conf::get('default_format');
 if (!file_exists(PATH_TEMPLATES."/index.$format.php")) {
