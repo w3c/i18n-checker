c81dfadc4043f3e49f8e017c241dadcfc66b6a99
r12a <ishida@w3.org>
2016-08-15 17:39:08 +0100

    about.php, substantially shortened and updated page

--- www/about.php
+++ www/about.php
@@ -11,7 +11,12 @@ $hideLangSelection = true;
 ?>
 <div class="about">
 
-<h2>About The W3C Internationalization (i18n) Checker</h2>
+<h2>About the W3C Internationalization (i18n) Checker</h2>
+<p>Getting internationalization features right at the beginning saves a
+  lot of time and trouble if you ever need to use your content in a
+  language-sensitive way in the future. However, the test will also throw
+  up issues that could cause you problems straight away, such as those
+  related to character encodings and to non-normalized class and id names. </p>
 <h3 id="TableOfContents">Table of contents</h3>
 
 <div id="toc" class="toc">
@@ -21,37 +26,25 @@ $hideLangSelection = true;
 		<li><a href="#credits">Credits</a></li>
 	</ol>
 </div>
-
 <h3 id="about">About this service</h3>
 
 <div class="bd compact">
 	<p>The <a href="<?php echo Conf::get('base_uri') ?>">W3C Internationalization Checker</a> is a
-	free service by <acronym title="World Wide Web Consortium">W3C</acronym>
-	that provides information about internationalization features of your
-	page, and provides advice on how to improve your use of
-    markup for the multilingual Web.</p>
-	<p>Getting internationalization features right at the beginning saves a
-	lot of time and trouble if you ever need to use your content in a
-	language-sensitive way in the future. However, the test will also throw
-	up issues that could cause you problems straight away, such as those
-	related to character encodings and to non-normalized class and id names.</p>
-
-	<p>The first part of the report for a page lists key
-	internationalization settings related to character encoding, language
-	declarations, text direction and class/id names. This information
-	includes that coming in HTTP headers, which can be particularly useful for
-	troubleshooting problems.</p>
-	<p>The second part of the report lists errors, warnings and helpful
-	suggestions based on your use (or not) of internationalization-related
-	markup. The report also points you to resources where you can read more
-	about the topic in question.</p>
-	<p><strong>The checker is currently a pre-release version, so please let us know about bugs and missing features using the <a href="feedback.html">feedback form</a></strong>. We already have plans to add a number of new features over the coming months, and to translate the user interface.</p>
+	free service by the <acronym title="World Wide Web Consortium">W3C</acronym>
+	that provides:</p>
+	<ul style="margin-left:3em;">
+	  <li>a table listing   key
+	    international settings for a page, such as character encoding, language
+	    declarations, and text direction.</li>
+	  <li>a list of errors, warnings and helpful
+	    suggestions about the page, with pointers to resources where you can learn more.</li>
+    </ul>
+	<p>The reports take into account both markup and HTTP headers, which can be particularly useful for
+    troubleshooting problems</p>
+	<p>Please let us know about bugs and missing features using the <a href="feedback.html">feedback form</a>. </p>
 	
-	<p>The information panel at the top of the page should work for most formats. In terms of issue reports, the checker currently targets HTML5, HTML4, and XHTML 1.0/1.1  (served as
-	text/html or application/xhtml+xml). It will soon produce issue reports for XHTML5 or Polyglot pages.</p>
-	<p>The hope is to eventually merge features of this checker with other
-	checkers and validators at the W3C, once it reaches an appropriate level
-	of maturity.</p>
+	<p>The information panel at the top of the page should work for most formats. The checker outputs tailored advice for HTML5, HTML4, and XHTML 1.0/1.1  (served as
+	text/html or application/xhtml+xml).</p>
 </div>
 
 <h3 id="others">References and other resources</h3>
@@ -84,17 +77,10 @@ $hideLangSelection = true;
 		organizations to make it possible to use Web technologies with
 		different languages, scripts, and cultures. It also provides articles
 		and other resources about Web internationalization.</li>
-		<li>The <a style="" href="/International/ig/">Internationalization
+		<li>The <a style="" href="https://www.w3.org/International/ig/">Internationalization
 		Interest Group</a> operates via a set of public mailing lists supporting the activity of
 		the Internationalization Working Groups. Anyone can participate in the
 		Interest Group by simply joining the mailing list.</li>
-		<li>The <a href="http://www.multilingualweb.eu/">MultilingualWeb
-		project</a> is exploring standards and best practices that support the
-		creation, localization and use of multilingual web-based information.
-		This project is funded by the European Commission through the ICT PSP
-		Grant Agreement No. 250500, and as part of the Competitiveness and
-		Innovation Framework Programme. Partners in the project are providing
-		feedback on the Internationalization Checker.</li>
 	</ul>
 </div>
 
@@ -119,9 +105,8 @@ $hideLangSelection = true;
 
 <h3 id="credits">Credits</h3>
 <div class="bd compact">
-	<p><a href="http://rishida.net/">Richard Ishida</a> and Thomas Gambet are developing the
-	checker at the W3C. The <a href="http://multilingualweb.eu/">MultilingualWeb
-	project</a> partners are also providing feedback on the checker.</p>
+	<p>Richard Ishida is maintaining the
+	checker at the W3C, using a code base developed by him and Thomas Gambet.</p>
 </div>
 
 </div>
